1055f3ac68ef2a4411d71c23e93e7d7f1c90fe49
nisequence <nicolesekol@gmail.com>
2023-03-18 23:16:57 -0400

    fixed bug which prevented 2nd team from guessing, began functionality to send users to next round

--- scripts/index.js
+++ scripts/index.js
@@ -25,17 +25,19 @@ let rightCat = 0; // iteration to skip thru json file by category
 let questionSelected = document.getElementsByClassName("questionSelected");
 let selectionSection = document.getElementsByClassName("superSecret");
 
+let teamUp = document.getElementById("teamUp");
+console.log(teamUp);
+let teamArray = [">>> Team A <<<", ">>> Team B <<<"];
+let tagTeam = 0;
+
 let aPts = document.getElementsByClassName("aPts")[0];
 //console.log(aPts);
 let bPts = document.getElementsByClassName("bPts")[0];
 //console.log(bPts);
 let aPtsVar = 0;
 let bPtsVar = 0;
-let teamUp = document.getElementById("teamUp");
-console.log(teamUp);
 
 let next1 = document.getElementById("next1");
-//! needs to be enabled when A or B reaches 15,000 points or when all questions are selected
 let pass1 = document.getElementById("pass1");
 let guess1 = document.getElementById("guess1");
 let ansInput = document.getElementById("answerBox");
@@ -44,7 +46,6 @@ console.log(ansInput);
 let parentCat;
 let pointValue;
 let cQuestionTest;
-let cQuestion;
 let startCategory;
 
 let qCardCollection = document.getElementsByClassName("questionCard");
@@ -56,29 +57,22 @@ for (let i = 0; i < newQCardArray.length; i++) {
         //console.log(parentCat);
         pointValue = newQCardArray[i].innerText; // e.g. "200"
         cQuestionTest = `We need a question for ${parentCat} that would give the user ${pointValue} if answered correctly.`;
-        //! console.log(cQuestionTest); test
         if (parentCat == "questions1") {
-            console.log("This is from cat1");
             startCategory = 0;
             findQuestion(startCategory, pointValue);
         } else if (parentCat == "questions2") {
-            console.log("This is from cat2");
             startCategory = 10;
             findQuestion(startCategory, pointValue);
         } else if (parentCat == "questions3") {
-            console.log("this is from cat3");
             startCategory = 20;
             findQuestion(startCategory, pointValue);
         } else if (parentCat == "questions4") {
-            console.log("this is from cat4");
             startCategory = 30;
             findQuestion(startCategory, pointValue);
         } else if (parentCat == "questions5") {
-            console.log("this is from cat5");
             startCategory = 40;
             findQuestion(startCategory, pointValue);
         } else if (parentCat == "questions6") {
-            console.log("this is from cat6");
             startCategory = 50;
             findQuestion(startCategory, pointValue);
         } else {
@@ -88,38 +82,6 @@ for (let i = 0; i < newQCardArray.length; i++) {
     }
 }
 
-/* * each q box w/ id
-let cat1q200 = document.getElements
-let cat1q400 = document.getElement;
-let cat1q600 = document.getElement;
-let cat1q800 = document.getElement;
-let cat1q1000 = document.getElement;
-let cat2q200 = document.getElement;
-let cat2q400 = document.getElement;
-let cat2q600 = document.getElement;
-let cat2q800 = document.getElement;
-let cat2q1000 = document.getElement;
-let cat3q200 = document.getElement;
-let cat3q400 = document.getElement;
-let cat3q600 = document.getElement;
-let cat3q800 = document.getElement;
-let cat3q1000 = document.getElement;
-let cat4q200 = document.getElement;
-let cat4q400 = document.getElement;
-let cat4q600 = document.getElement;
-let cat4q800 = document.getElement;
-let cat4q1000 = document.getElement;
-let cat5q200 = document.getElement;
-let cat5q400 = document.getElement;
-let cat5q600 = document.getElement;
-let cat5q800 = document.getElement;
-let cat5q1000 = document.getElement;
-let cat6q200 = document.getElement;
-let cat6q400 = document.getElement;
-let cat6q600 = document.getElement;
-let cat6q800 = document.getElement;
-let cat6q1000 = document.getElement;
-*/
 let qSource = "scripts/questions.json";
 async function fetchCategories() {
     fetch(qSource)
@@ -145,31 +107,39 @@ async function findQuestion(cat, points) {
         let res = await fetch(qSource);
         let results = await res.json();
         let data = results.placeholderQuestions[howToFind];
-        console.log(data);
+        console.log(data); //! for testing purposes
         selectionSection[0].id = ""; // this will make it impossible for user to select new question
         //! add onclick event for selectionSection that tells user to answer or pass
         questionSelected[0].id = "";
         questionSelected[0].innerText = data.question;
         enablePass();
         guess1.onclick = () => {
-            console.log(ansInput.value);
-            console.log(data.answer);
-            console.log(questionSelected);
-            console.log(selectionSection);
+            console.log(ansInput.value); //! testing
+            console.log(data.answer); //! testing
             if (ansInput.value == data.answer) {
                 console.log("Yay! You got it!");
                 addPoints(points);
-                questionSelected[0].id = "invis";
-                selectionSection[0].id = "hideIt";
+                hideTheQuestion();
+                endGuessing();
             } else if (ansInput.value == "") {
                 let noInputError = "User did not input anything.";
-                console.log(noInputError);
+                console.log(noInputError); //! send alert to users instead
             } else { // wrong answer
                 console.log("Nice try...");
                 subtractPoints(points);
-                teamUp.textContent = ">>> Team B <<<"; //! need to make this & Team A variables and iterate to/from - only 1 chance per team
-
-                //! maybe do more fancy stuff later if time
+                tagTeam++;
+                console.log(tagTeam);
+                if (tagTeam > 1) {
+                    tagTeam = tagTeam - 2; // reset
+                    teamUp.textContent = teamArray[tagTeam];
+                    //! maybe do more fancy stuff later if time
+                    hideTheQuestion();
+                    endGuessing();
+                } else {
+                    console.log(teamArray[tagTeam]);
+                    teamUp.textContent = teamArray[tagTeam];
+                    //! maybe do more fancy stuff later if time
+                }
             }
         }
     } catch(err) {
@@ -179,10 +149,23 @@ async function findQuestion(cat, points) {
 
 async function enablePass() {
     pass1.onclick = () => {
-        teamUp.textContent = ">>> Team B <<<";
-        //! need to make this & Team A variables and iterate to/from - only 1 chance to pass
-        
-        //! maybe do more fancy stuff later if time
+        tagTeam++;
+        console.log(tagTeam);
+        if (tagTeam > 1) {
+            tagTeam = tagTeam - 2; // reset
+            teamUp.textContent = teamArray[tagTeam];
+            //! maybe do more fancy stuff later if time
+            hideTheQuestion();
+            endGuessing();
+        } else {
+            console.log(teamArray[tagTeam]);
+            teamUp.textContent = teamArray[tagTeam];
+            //! maybe do more fancy stuff later if time
+        }
+        //  only 1 chance to pass
+        pass1.onclick = () => {
+            // do nothing 2nd time
+        }
     }
 }
 
@@ -191,15 +174,9 @@ async function addPoints(pointAmount) {
     if (teamUp.textContent == ">>> Team A <<<") {
         aPtsVar = parseInt(aPtsVar + pointDigit);
         aPts.innerText = aPtsVar + " PTS";
-        guess1.onclick = () => { //! need to move this so that guess1 button works for Team B
-            console.log("Do nothing");
-        }
     } else if (teamUp.textContent == ">>> Team B <<<") {
         bPtsVar = parseInt(bPtsVar + pointDigit);
         bPts.innerText = bPtsVar + " PTS";
-        guess1.onclick = () => { //! need to move this so that guess1 button works for Team B
-            console.log("Do nothing");
-        }
     } else {
         console.error("I don't know what team that is!");
     }
@@ -210,23 +187,36 @@ async function subtractPoints(pointAmount) {
     if (teamUp.textContent == ">>> Team A <<<") {
         aPtsVar = parseInt(aPtsVar - pointDigit);
         aPts.innerText = aPtsVar + " PTS";
-        guess1.onclick = () => {
-            console.log("Do nothing");
-        }
     } else if (teamUp.textContent == ">>> Team B <<<") {
         bPtsVar = parseInt(bPtsVar - pointDigit);
         bPts.innerText = bPtsVar + " PTS";
-        guess1.onclick = () => {
-            console.log("Do nothing");
-        }
     } else {
         console.error("I don't know what team that is!");
     }
 }
 
-async function notGuessed() { //! need to call this after both teams get it wrong
+async function hideTheQuestion() { // call this after both teams get it wrong and/or pass
     questionSelected[0].id = "invis";
     selectionSection[0].id = "hideIt";
+}
+
+async function endGuessing() {
+    guess1.onclick = () => {
+        console.log("Do nothing");
+    }
+    checkIfLimitReached();
+}
+
+async function checkIfLimitReached() { //! need to test this function
+    if (aPtsVar >= 15000 || bPtsVar >= 15000 || newQCardArray[0] == undefined) {
+        console.log("maxed out pts for this round or used all questions");
+        next1.onclick = () => {
+            console.log("Hi users, you can move on now.");
+            //! need to alert users (not in console)
+            console.log(`I need to send the users to a URL to page two with the values of ${aPtsVar} for Team A and ${bPtsVar} for Team B.`);
+            //! don't forget to actually do this
+        }
+    }
 }
 // --------------------
 // on page load --->
